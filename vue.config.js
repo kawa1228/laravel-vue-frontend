2f2a9e943bd7b381b714c3e2a44b6bf9f6d8ee33
kawa1228 <sxa.1105bo@gmail.com>
2020-01-03 22:51:03 +0900

    vueのproxyの設定を変更

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     // options...
     devServer: {
+        public: 'localhost',
         port: 8888,
         disableHostCheck: true,
-    },
-  }
+        proxy: {
+            'api/': {
+                target: 'http://127.0.0.1:10080'
+            }
+        }
+    }
+}
